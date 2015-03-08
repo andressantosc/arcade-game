3a89ac8007d59b69654895b9bb20bef172f457e9
Andres Santos <andres.santos188@gmail.com>
2015-03-08 02:47:29 -0600

    created empty allEnemies array and corrected an error in Enemy function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function(){
+var Enemy = function(x, y){
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = y;
-    this.y = x;
+    this.x = x;
+    this.y = y;
     this.speed = 60;
 }
 
@@ -59,6 +59,8 @@ Player.prototype.handleInput = function(e) {
 // Place the player object in a variable called player
 
 var player = new Player();
+
+var allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
