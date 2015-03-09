9951c7e4dae6cd6454999da1dac18afbc0e10297
Andres Santos <andres.santos188@gmail.com>
2015-03-09 02:08:58 -0600

    Updated README files

---
+++ js/README.js
@@ -0,0 +1,15 @@
+
+/* 
+To start the game, you only need to open index.html, 
+found in the main folder.
+
+Use arrow keys to move the player.
+You need to avoid hiting bugs.
+The object of the game is to get to the water.
+
+I used these websites for research:
+http://www.w3schools.com/jsref/jsref_random.asp
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
+http://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
+
+*/
